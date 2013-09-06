8748139c6bd1ea5a8bcbfe3b2d390a6c94c9a6cb
romecode <r.onosovski@gmail.com>
2013-09-06 18:05:04 -0400

    Roman Fri Sep 6th users: felipe, sean, newman, roman password: moqui Screen Hierarchy

---
+++ runtime/component/user/script/TimeclockScript.groovy
@@ -0,0 +1,7 @@
+// Find current userId
+
+
+    // Create TC_new once TC_done takes place
+//    def createTcNew = ec.entity.makeValue("Timeclock")
+//    createTcNew.set(userId, ec.user.getUserId())
+//    createTcNew.create()
