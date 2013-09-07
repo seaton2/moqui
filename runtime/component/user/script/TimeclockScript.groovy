7aae6ca58d48a2561c8e2925a5557b84c9e93d41
romecode <r.onosovski@gmail.com>
2013-09-07 11:20:30 -0400

    Added Standalone Time clock application to Login screen

--- runtime/component/user/script/TimeclockScript.groovy
+++
@@ -1,7 +0,0 @@
-// Find current userId
-
-
-    // Create TC_new once TC_done takes place
-//    def createTcNew = ec.entity.makeValue("Timeclock")
-//    createTcNew.set(userId, ec.user.getUserId())
-//    createTcNew.create()
